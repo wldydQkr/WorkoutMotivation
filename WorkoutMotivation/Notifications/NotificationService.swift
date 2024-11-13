5c024274b7edd8ee905651783e3083164535d74f
wldydQkr <wlqkr1010@gmail.com>
2024-11-13 23:09:10 +0900

    ...

--- WorkoutMotivation/Notifications/NotificationService.swift
+++ WorkoutMotivation/Notifications/NotificationService.swift
@@ -28,13 +28,3 @@ struct NotificationService {
     }
   }
 }
-
-//class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
-//  func userNotificationCenter(
-//    _ center: UNUserNotificationCenter,
-//    willPresent notification: UNNotification,
-//    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-//  ) {
-//    completionHandler([.banner, .sound])
-//  }
-//}
